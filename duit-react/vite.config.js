c1d56f025baeca5b82f7f4648e9cf252e8e34d13
Vishnu <vprabh@pdisoftware.com>
2021-12-18 03:19:51 -0600

    pwa done

--- duit-react/vite.config.js
+++ duit-react/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -36,6 +37,6 @@ export default () => {
                 },
             },
         },
-        plugins: [react()],
+        plugins: [react(), VitePWA()],
     });
 };
